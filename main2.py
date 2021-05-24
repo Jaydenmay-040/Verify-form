d1e6ae8debddf5898e17d2ee995162ba94f7cafb
Jaydenmay-040 <82215016+Jaydenmay-040@users.noreply.github.com>
2021-05-24 15:43:36 +0200

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk()
+window.title("News Feed")
+window.geometry("300x155")
+
+lbl = Label(window, text="Welcome!", font='monospace 21')
+lbl.place(x=17.5, y=50)
+window.mainloop()
